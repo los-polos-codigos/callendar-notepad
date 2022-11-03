b912c858b0b67aaeca9d4f8730abe0178318c4a5
Szymon Brud <szymonbrud@stellarity.co>
2022-11-03 22:52:26 +0100

    Update eslint config

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
-import React from "react";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-
-export default function App() {
-    return (
-        <View style={styles.container}>
-            <Text>First app</Text>
-            <StatusBar style="auto" />
-        </View>
-    );
-}
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        _backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-    },
+  container: {
+    flex: 1,
+    _backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>First app</Text>
+      <StatusBar />
+    </View>
+  );
+}
